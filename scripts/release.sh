2a79a00be4a9ac58f29265aacbd2d3bd8960b64e
Oleh Polishchuk <oleh.polishchuk.ck@gmail.com>
2022-01-30 22:58:33 +0200

    Update package

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,6 +6,6 @@ npm run build:cleanup
 
 git add .
 git commit -m 'Update package'
-git tag -a -m 'My first action release' v1.13
+git tag -a -m 'My first action release' v1.14
 git push --follow-tags
 git push
